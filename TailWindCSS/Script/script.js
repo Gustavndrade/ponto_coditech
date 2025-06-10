163d4c8bc0e62fdc54d0fa5ef1f43e117eadc4c8
Gustavo Andrade <abguto.06@gmail.com>
2025-06-09 21:07:13 -0300

    dash

---
+++ TailWindCSS/Script/script.js
@@ -0,0 +1,9 @@
+    IMask(document.getElementById('CPF'), { mask: '000.000.000-00' });
+    IMask(document.getElementById('RG'), { mask: '0000000' });
+    IMask(document.getElementById('telefone'), { mask: '(00) 00000-0000' });
+
+    const formulario = document.querySelector("#formulario");
+    
+    function proximo() {
+    
+    }
