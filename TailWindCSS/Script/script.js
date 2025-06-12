577ebea80915c89b99b82f88b5e2b8823520eca0
Gustavo Andrade <abguto.06@gmail.com>
2025-06-12 15:03:43 -0300

    tentativas tristes e frustadas de arrumar o slide com translateX

--- TailWindCSS/Script/script.js
+++ TailWindCSS/Script/script.js
@@ -2,19 +2,17 @@
     IMask(document.getElementById('RG'), { mask: '0000000' });
     IMask(document.getElementById('telefone'), { mask: '(00) 00000-0000' });
 
-    const formulario = document.querySelector("#formulario");
-    
     var passoatual = 0;
     const container = document.getElementById("container");
     const passo = container.children
 
     function proximo_passo() {
-        const tamanho = passo[0].offsetWidith;
+        const tamanho = passo[0].offsetWidth;
         container.style.transform = `translateX(-${passoatual * tamanho}px)`;
     }
 
     document.querySelector("#proximo").addEventListener("click", () => {
-        if (passoatual < passo.length - 1){
+        if (passoatual < passo.length -1){
             passoatual++;
             proximo_passo();
         }
@@ -23,7 +21,7 @@
     document.querySelector("#voltar").addEventListener("click", () => {
         if (passoatual > 0){
             passoatual--;
-            proximo_passo
+            proximo_passo();
         }
     })
   
